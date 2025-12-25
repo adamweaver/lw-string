55e52771b585d13a23728d142a51e5122493e209
Adam Weaver <adam@cleversure.com.au>
2025-12-25 08:16:28 +0800

    reformat

--- package.lisp
+++ package.lisp
@@ -5,13 +5,12 @@
 (defpackage :english 
   (:use :cl)
   (:export "VOWELP" "PLURALISE-WORD" "FORMAT-PHONE-NUMBER" "NORMALISE-PHONE-NUMBER" "FORMAT-ABN" "MAKE-ABN" 
-	   "MAKE-PHONE-NUMBER" "MAKE-EMAIL-ADDRESS"))
+           "MAKE-PHONE-NUMBER" "MAKE-EMAIL-ADDRESS"))
 
 (defpackage :string
   (:use :cl)
-  (:export 
-    case* strcat strcat-adjacent join left-pad urlify random-alpha-ascii-string starts-with ends-with & && match
-    compose partial partial* reverse-c-byte-order group* flatten intersperse mklist uncons nsubseq)
+  (:export "case*" "strcat" "strcat-adjacent" "join" "left-pad" "urlify" "random-alpha-ascii-string" "starts-with" "ends-with" "&" "&&" "match"
+           "compose" "partial" "partial*" "reverse-c-byte-order" "group*" "flatten" "intersperse" "mklist" "uncons" "nsubseq")
 
 
 
